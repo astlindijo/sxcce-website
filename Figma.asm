08458fbf01dffcf7c86e88523d7c0ddff1807a9a
Astlin Dijo <astlindijo@icloud.com>
2025-12-28 22:48:17 +0530

    Update Figma.asm

--- Figma.asm
+++ Figma.asm
@@ -1,1 +1,16 @@
+.section __TEXT,__text
+.global _main
+.extern _printf
+
+_main:
+    adrp    x0, message@PAGE
+    add     x0, x0, message@PAGEOFF
+    bl      _printf
+
+    mov     x0, #0
+    ret
+
+.section __DATA,__data
+message:
+    .asciz  "Kali Linux !\n"
 
